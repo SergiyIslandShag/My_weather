2824814a8834278c3fb2f1b210b61e0368a6e556
SergiySergiyIslandShag <sergiykoziar13@gmail.com>
2024-12-16 20:50:57 +0200

    fix

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
+const API_KEY = '';
+let currentData = {};
 
+function showTodayForecast() {
+    document.getElementById('today-content').style.display = 'block';
+    document.getElementById('forecast-content').style.display = 'none';
+}
+
+function showForecast() {
+    document.getElementById('forecast-content').style.display = 'block';
+    document.getElementById('today-content').style.display = 'none';
+}
+
+function handleSearch(event) {
+    event.preventDefault();
+    const searchInput = document.querySelector('#search-input').value;
+    fetchWeatherData(searchInput);
+}
+
+
+showTodayForecast();
